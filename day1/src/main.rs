d448184a727c8de3c3e73ca272bf37ad20b91afd
Robert Hales <rbthales@gmail.com>
2023-12-01 16:27:44 +0000

    Day 1 - Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,32 @@
 fn main() {
     let input = include_str!("input.txt");
 
-    let answer: usize = input.lines().map(|l| {
-        let first_index = l.find(char::is_numeric).expect("could not find numeric char");
-        let last_index = l.rfind(char::is_numeric).expect("could not find numeric char");
+    let string_digits = [
+        "one", 
+        "two",
+        "three", 
+        "four", 
+        "five", 
+        "six",  
+        "seven", 
+        "eight",
+        "nine",
+        "ten",
+    ];
 
-        let number = format!("{}{}", l.chars().nth(first_index).unwrap(), l.chars().nth(last_index).unwrap()).parse::<usize>().unwrap();
-        number
+    let answer: u32 = input.lines().map(|line| {
+        let mut digits = line.chars().enumerate().filter_map(|(i, c)|{
+            if c.is_numeric() {
+                Some(c.to_digit(10).expect("could not conver char to digit"))
+            } else {
+                string_digits.iter().enumerate().find_map(|(val, digit_string)| line[i..].starts_with(digit_string).then_some((val + 1) as u32))
+            }
+        });
+        
+        let first_digit = digits.next().expect("no first digit");
+        first_digit * 10 + digits.last().unwrap_or(first_digit)
     }).sum();
 
     dbg!(answer);
 }
+
