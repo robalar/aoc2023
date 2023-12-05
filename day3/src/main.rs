68e414d9089d385459317149665a982542da5681
Robert Hales <rbthales@gmail.com>
2023-12-05 14:44:52 +0000

    Day 3 - Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,58 +1,84 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 fn main() {
     let input = include_str!("input.txt");
 
-    let grid = input.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let grid = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 
     let height = grid.len();
     let width = grid[0].len();
 
-    let mut sum = 0;
+    let mut gear_map = HashMap::new();
+
     for i in 0..height {
         let mut current_number = String::new();
-        let mut adjacent_to_symbol = false;
-        
+        let mut adjacent_to_gear = None;
 
         for j in 0..width {
             let c = grid[i][j];
             if c.is_numeric() {
                 current_number.push(c);
-                adjacent_to_symbol |= has_adjacent_symbol(&grid, i, j);
+                if let Some(gear) = has_adjacent_gear(&grid, i, j) {
+                    adjacent_to_gear.get_or_insert(gear);
+                } 
             } else {
-                if adjacent_to_symbol && !current_number.is_empty() {
-                    sum += current_number.parse::<u32>().expect("could not parse number");
+                if let Some(coords) = adjacent_to_gear {
+                    gear_map.entry(coords).or_insert(vec![]).push(
+                        current_number
+                            .parse::<u32>()
+                            .expect("could not parse number"),
+                    )
                 }
 
                 // reset
                 current_number = String::new();
-                adjacent_to_symbol = false;
+                adjacent_to_gear = None;
             }
         }
 
-        if adjacent_to_symbol && !current_number.is_empty() {
-            sum += current_number.parse::<u32>().expect("could not parse number");
+        if let Some(coords) = adjacent_to_gear {
+            gear_map
+                .entry(coords)
+                .or_insert(vec![])
+                .push(current_number.parse().expect("could not parse number"))
         }
     }
 
-    dbg!(sum);
+    dbg!(&gear_map);
 
+    let answer: u32 = gear_map
+        .values()
+        .filter_map(|g| {
+            if g.len() == 2 {
+                Some(g[0] * g[1])
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    dbg!(answer);
 }
 
 
-fn has_adjacent_symbol(grid: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+fn has_adjacent_gear(grid: &Vec<Vec<char>>, i: usize, j: usize) -> Option<(usize, usize)> {
     for (detla_i, delta_j) in (-1..=1).cartesian_product(-1..=1) {
         let x = j as i32 + delta_j;
         let y = i as i32 + detla_i;
 
         let adj_c = grid.get(y as usize).and_then(|r| r.get(x as usize));
-        
+
         if let Some(adj_c) = adj_c {
-            if !adj_c.is_numeric() && adj_c != &'.' {
-                return true;
+            if adj_c == &'*' {
+                return Some((y as usize, x as usize));
             }
         }
     }
 
-    return false;
+    return None;
 }
